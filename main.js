7742f00335df029e45e3b6355e6ebb4cd625c0f7
siphokazi <naledimuhauli@gmail.com>
2024-06-21 00:22:43 +0300

    added a shopping cart section and made it responsive with js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-function popUp () {
-    alert ('hello');
+// when user clicks 'add to cart', add item iton cart and display number
+//when user clicks 'add to cart' again, increase number by 1
+//when user clicks the cart , display the cart section
+
+function displayCart(){
+    let cart = document.querySelector('#cart');
+    let body = document.querySelector('body');
+
+    body.classList.toggle('showCart')
+}
+    cart.addEventListener('click', displayCart())
+
+
+
+
+function handleCart(){
+    let numberInCart = document.querySelector('.number');
+    numberInCart;
+    console.log(typeof numberInCart);
 }
