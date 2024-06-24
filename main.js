516a3d09e7c6e3cb18d0b875133d9311130ef93d
siphokazi <naledimuhauli@gmail.com>
2024-06-24 21:32:29 +0300

    changed the js structure

--- main.js
+++ main.js
@@ -1,131 +1,123 @@
-// when user clicks cart icon, show the shopping cart
+const openShopping = document.querySelector('.cart-icon');
+const closeShopping = document.querySelector('.closeShopping');
+const list = document.querySelector('.list');
+const listCard = document.querySelector('.listCard');
+const total = document.querySelector('.total');
+const body = document.querySelector('body');
+const quantity = document.querySelector('.number');
 
-let cart = document.querySelector('#cart');
+openShopping.addEventListener('click', () =>{
+    body.classList.add('active');
+});
 
-function displayCart(){
-     let body = document.querySelector('body');
-    body.classList.toggle('showCart')
+closeShopping.addEventListener('click', () =>{
+    body.classList.remove('active');
+});
+
+let products = [{
+    id: '1',
+    name: 'Scented Bliss',
+    image: 'images/perfume-9.webp',
+    price: 1200 
+},{
+    id : '2',
+    name: 'Fragrance Fusion',
+    image: 'images/perfume-2.jfif',
+    price: 1000
+},{
+    id: '3',
+    name: 'Aromaavenue',
+    image: 'images/perfume-3.jfif',
+    price: 1500
+},{
+    id: '4',
+    name: 'Fragrance Finesse',
+    image: 'images/perfume-10.jfif',
+    price: 1700
+},{
+    id: '5',
+    name: 'Noir Absolu',
+    image: 'images/perfume-11.jfif',
+    price: 1200
+},{
+    id: '6',
+    name: 'Enchante`',
+    image: 'images/perfume-12.jfif',
+    price: 2100
+}];
+
+let listCards = [];
+
+const initApp = () =>{
+    products.forEach((value, key) => {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCart(${key})">Add To Cart</button>
+        `;
+        list.appendChild(newDiv);
+    });
 }
-    cart.addEventListener('click', displayCart());
 
-    
-    // add the item into the cart
-   let cartItems = [
-    {
-        productId: '1',
-        name: 'Scented Bliss',
-        image: 'images/perfume-9.webp',
-        price: 1200,
-        quantity: 1 
-    },{
-        productId : '2',
-        name: 'Fragrance Fusion',
-        image: 'images/perfume-2.jfif',
-        price: 1000,
-        quantity: 1
-    },{
-        productId: '3',
-        name: 'Aromaavenue',
-        image: 'images/perfume-3.jfif',
-        price: 1500,
-        quantity: 1
-    },{
-        productId: '4',
-        name: 'Fragrance Finesse',
-        image: 'images/perfume-10.jfif',
-        price: '1700',
-        quantity: 1
-    },{
-        productId: '5',
-        name: 'Noir Absolu',
-        image: 'images/perfume-11.jfif',
-        price: 1200,
-        quantity: 1
-    },{
-        productId: '6',
-        name: 'Enchante`',
-        image: 'images/perfume-12.jfif',
-        price: 2100,
-        quantity: 1
+initApp();
+
+const addToCart = (key) =>{
+    if (listCards[key] == null) {
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    } else {
+        listCards[key].quantity += 1;
+        listCards[key].price = listCards[key].quantity * products[key].price;
     }
-   ];
+    reloadCart();
+};
 
-    let addButton = document.querySelectorAll('.add-btn');
+const reloadCart = () =>{
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
 
-    addButton.forEach(button => {
-        button.addEventListener('click', () => {
-         let productId = button.dataset.id;
-        
-        //  check if the product is in the cart
-         let matchingItem;
+    listCards.forEach((value, key) => {
+        if (value != null) {
+            totalPrice += value.price;
+            count += value.quantity;
 
-          cartItems.forEach((item) => {
-             if(productId === item.productId) {
-                matchingItem = item;
-             }
-          });
-          //if it is, increase the mathchingitems by 1
-
-          if(matchingItem){
-             matchingItem.quantity += 1;
-          } else{
-                // If not, add the new product to the cart
-             cartItems.push({
-                productId: productId,
-                quantity: 1
-             });
-
-          };
-
-          //increase the cart number on the cart icon
-
-        //   let cartQuantity = 0;
-
-        //   cartItems.forEach((item)=> {
-        //     cartQuantity += item.quantity;
-        //   } );
-        //   document.querySelector('.number')
-        //   .innerHTML = cartQuantity;
-
-             console.log(cartItems);
-            //  console.log(cartQuantity);
-
-     handleCart();
-     handleShoppingCart();
-        });
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="${value.image}"></div>
+                <div class="cardTitle">${value.name}</div>
+                <div class="cardPrice">${value.price.toLocaleString()}</div>
+                <div>
+                    <button style="background-color: #560bad" class="cardButton" onClick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button style="background-color: #560bad" class="cardButton" onClick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>
+            `;
+            listCard.appendChild(newDiv);
+        }
     });
 
-     let cartSummaryHtml = '';
-    function handleShoppingCart(){
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
 
-   
-
-    cartItems.forEach((cartItem) =>{
-
-         cartSummaryHtml += `
-         <div class="items">
-    <div class="image">
-        <img src="${cartItem.image}" alt="${cartItem.name}">
-    </div>
-    <div class="name">
-        NAME: ${cartItem.name}
-    </div>
-    <div class="price">
-        PRICE: ${cartItem.price}
-        <br>
-        Quantity:${cartItem.quantity} 
-    </div> `;
-        });
-            
-   document.querySelector('.js-order-summary').innerHTML = cartSummaryHtml;
+    // Close cart if empty and set quantity to 0
+    if (count === 0) {
+        body.classList.remove('active');
+        quantity.innerText = '0';
+    }
 };
-    console.log(cartSummaryHtml);
+
+const changeQuantity = (key, newQuantity) => {
+    if (newQuantity <= 0) {
+        delete listCards[key];
+    } else {
+        listCards[key].quantity = newQuantity;
+        listCards[key].price = newQuantity * products[key].price;
+    }
+    reloadCart();
+};
 
 
-function handleCart(){
-    let numberInCart = document.querySelector('.number');
-    let cartNumber = Number(numberInCart.innerHTML);
-
-    cartNumber += 1;
-    numberInCart.innerHTML = cartNumber;
-}
--- script.js
+++ script.js
@@ -1,57 +1,123 @@
-//save the product information in an array
- /*
-    const products = [{
-        id: '1',
-        name: 'Scented Bliss',
-        image: 'images/perfume-9.webp',
-        price: 1200 
-    },{
-        id : '2',
-        name: 'Fragrance Fusion',
-        image: 'images/perfume-2.jfif',
-        price: 1000
-    },{
-        id: '3',
-        name: 'Aromaavenue',
-        image: 'images/perfume-3.jfif',
-        price: 1500
-    },{
-        id: '4',
-        name: 'Fragrance Finesse',
-        image: 'images/perfume-10.jfif',
-        price: '1700'
-    },{
-        id: '5',
-        name: 'Noir Absolu',
-        image: 'images/perfume-11.jfif',
-        price: 1200
-    },{
-        id: '6',
-        name: 'Enchante`',
-        image: 'images/perfume-12.jfif',
-        price: 2100
+const openShopping = document.querySelector('.cart-icon');
+const closeShopping = document.querySelector('.closeShopping');
+const list = document.querySelector('.list');
+const listCard = document.querySelector('.listCard');
+const total = document.querySelector('.total');
+const body = document.querySelector('body');
+const quantity = document.querySelector('.number');
+
+openShopping.addEventListener('click', () =>{
+    body.classList.add('active');
+});
+
+closeShopping.addEventListener('click', () =>{
+    body.classList.remove('active');
+});
+
+let products = [{
+    id: '1',
+    name: 'Scented Bliss',
+    image: 'images/perfume-9.webp',
+    price: 1200 
+},{
+    id : '2',
+    name: 'Fragrance Fusion',
+    image: 'images/perfume-2.jfif',
+    price: 1000
+},{
+    id: '3',
+    name: 'Aromaavenue',
+    image: 'images/perfume-3.jfif',
+    price: 1500
+},{
+    id: '4',
+    name: 'Fragrance Finesse',
+    image: 'images/perfume-10.jfif',
+    price: 1700
+},{
+    id: '5',
+    name: 'Noir Absolu',
+    image: 'images/perfume-11.jfif',
+    price: 1200
+},{
+    id: '6',
+    name: 'Enchante`',
+    image: 'images/perfume-12.jfif',
+    price: 2100
+}];
+
+let listCards = [];
+
+const initApp = () =>{
+    products.forEach((value, key) => {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCart(${key})">Add To Cart</button>
+        `;
+        list.appendChild(newDiv);
+    });
+}
+
+initApp();
+
+const addToCart = (key) =>{
+    if (listCards[key] == null) {
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    } else {
+        listCards[key].quantity += 1;
+        listCards[key].price = listCards[key].quantity * products[key].price;
     }
-    ];
+    reloadCart();
+};
 
-    // Generate the html
-    let productsHtml = '';
+const reloadCart = () =>{
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
 
-     products.forEach((product) => {
-          productsHtml += `
-          <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-          <div class="col-4 product">
-            <div class="card mb-4 rounded-3 shadow-sm">
-              <div class="card-body">
-                <img src="${product.image}" alt="" class="image">
-                <h4 class="my-0 fw-normal">${product.name}</h4>
-                 <h1 class="card-title pricing-card-title">${product.price}</h1>
-                <button type="button" class="w-100 btn btn-lg btn-dark add-btn" data-product-name="${product.id}">Add to cart</button>
-              </div>
-            </div>
-          </div>
-          </div>
-          `
-     });
+    listCards.forEach((value, key) => {
+        if (value != null) {
+            totalPrice += value.price;
+            count += value.quantity;
 
-     console.log(document.querySelector('.js-body')
-     .innerHTML = productsHtml) */
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="${value.image}"></div>
+                <div class="cardTitle">${value.name}</div>
+                <div class="cardPrice">${value.price.toLocaleString()}</div>
+                <div>
+                    <button style="background-color: #560bad" class="cardButton" onClick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button style="background-color: #560bad" class="cardButton" onClick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>
+            `;
+            listCard.appendChild(newDiv);
+        }
+    });
+
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+
+    // Close cart if empty and set quantity to 0
+    if (count === 0) {
+        body.classList.remove('active');
+        quantity.innerText = '0';
+    }
+};
+
+const changeQuantity = (key, newQuantity) => {
+    if (newQuantity <= 0) {
+        delete listCards[key];
+    } else {
+        listCards[key].quantity = newQuantity;
+        listCards[key].price = newQuantity * products[key].price;
+    }
+    reloadCart();
+};
+
+
